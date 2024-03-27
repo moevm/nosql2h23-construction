42b452e452436831b0f327345c2154c8e14c2c18
Roman Karabanov <roman.karabanoff@mail.ru>
2024-03-27 19:31:52 +0300

    add func to materials

--- database/materials.js
+++ database/materials.js
@@ -15,8 +15,39 @@ async function mongoGetMaterials(){
     return await MaterialModel.find({}).lean();
 }
 
+async function mongoUseMaterialsPositions(id, qty){
+    MaterialModel.updateOne(
+        {"_id": id},
+        {$inc: {[`quantity.${0}`]: qty}}
+        )
+        .then(result => {console.log(`Upd doc`);})
+        .catch(error => {console.error(`Err`)});
+}
+
+async function mongoUseMaterialsStock(id, qty){
+    MaterialModel.updateOne(
+        {"_id": id},
+        {$inc: {[`quantity.${1}`]: qty}}
+        )
+        .then(result => {console.log(`Upd doc`);})
+        .catch(error => {console.error(`Err`)});
+}
+
+
+async function mongoAddMaterial(material){
+    MaterialModel.insertMany([{
+        name: material.name,
+        type: material.type,
+        unity: material.unit,
+        quantity: [0, 0],
+        description: material.description
+    }]).then((result) => {
+        console.log("result: ", result);
+    })
+}
+
 async function mongoFindMaterial(id){
     return MaterialModel.findById(id).lean()
 } 
 
-module.exports = { mongoGetMaterials, mongoFindMaterial}
+module.exports = { mongoGetMaterials, mongoFindMaterial, mongoAddMaterial, mongoUseMaterialsPositions, mongoUseMaterialsStock}
